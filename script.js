5caf2a061da55a596aa2fec460af8f8287cec76f
natcortina <natcortinab@gmail.com>
2023-11-06 21:16:50 -0500

    added new animations

--- script.js
+++ script.js
@@ -10,8 +10,13 @@ let personas = {
         name: "Louis",
         photo: "img/louis.gif"
      
-    } 
+    } ,
 
+    sonia: {
+        name: "Sonia",
+        photo: "img/sonia.gif"
+     
+    } 
 }
 
 //vilma
@@ -21,3 +26,23 @@ document.querySelector(".vilma-image").setAttribute("src",personas.vilma.photo)
 //louis
 document.querySelector('.louisname').innerHTML = personas.louis.name
 document.querySelector(".louis-image").setAttribute("src",personas.louis.photo)
+
+//louis
+document.querySelector('.sonianame').innerHTML = personas.sonia.name
+document.querySelector(".sonia-image").setAttribute("src",personas.sonia.photo)
+
+
+function displayText() {
+    var text = document.getElementById("textField");
+    text.style.display = "block";
+  }
+
+  function displayText2() {
+    var text = document.getElementById("textField1");
+    text.style.display = "block";
+  }
+
+  function displayText3() {
+    var text = document.getElementById("textField2");
+    text.style.display = "block";
+  }
